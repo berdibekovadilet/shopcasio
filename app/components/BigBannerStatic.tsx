0290d336200a0064a2e8118be5d847b9fde15a3c
Adilet Berdibekov <berdibekovadilet@gmail.com>
2023-10-29 18:00:28 +0600

    add halloween banner

--- app/components/BigBannerStatic.tsx
+++ app/components/BigBannerStatic.tsx
@@ -1,16 +1,16 @@
 import {Link} from "~/components/Link";
-import Banner from "~/assets/banners/anniversary-40.jpeg"
+import Banner from "~/assets/banners/halloween3.jpg"
 
 export const BigBannerStatic = () => {
   return (
-    <Link to="anniversary-40">
+    <Link to="products/g-shock-ga-2200m-4a">
       <div
         className='relative overflow-hidden inline-block md:min-w-[700px] h-[531px] transition-all duration-[0.2s] ease-[ease] mb-6 hover:scale-[1.02] rounded-[20px]'>
         <img src={Banner} alt="Banner" className='w-full h-full object-cover'/>
         <div
           className='absolute left-6 bottom-8 rounded-md flex flex-col items-baseline justify-between gap-3 px-3 py-4 sm:px-4 md:px-6 bg-gradient-to-b dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast'>
           <h2 className="max-w-md text-3xl font-medium">
-            40-летний юбилей
+            Halloween с нами
           </h2>
           {/*<p className='text-xl font-medium'>40-летний юбилей</p>*/}
         </div>
