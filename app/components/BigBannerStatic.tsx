185c7a3367ef7bf1a7d425b2598a2b95ae940413
Adilet Berdibekov <berdibekovadilet@gmail.com>
2024-08-30 10:04:20 +0600

    change big_banner

--- app/components/BigBannerStatic.tsx
+++ app/components/BigBannerStatic.tsx
@@ -1,12 +1,12 @@
-import {Link} from "~/components/Link";
-import Banner from "~/assets/banners/casio50anniversary.png"
+import {Link} from '~/components/Link';
+// import Banner from "~/assets/banners/casio50anniversary.png"
+import Banner from '~/assets/banners/casio-event2.png';
 
 export const BigBannerStatic = () => {
   return (
     <Link to="/anniversary-50-casio">
-      <div
-        className='relative overflow-hidden inline-block md:min-w-[700px] h-[531px] transition-all duration-[0.2s] ease-[ease] mb-6 hover:scale-[1.02] rounded-[20px]'>
-        <img src={Banner} alt="Banner" className='w-full h-full object-cover'/>
+      <div className="relative overflow-hidden inline-block md:min-w-[700px] h-[531px] transition-all duration-[0.2s] ease-[ease] mb-6 hover:scale-[1.02] rounded-[20px]">
+        <img src={Banner} alt="Banner" className="w-full h-full object-cover" />
         {/*<div*/}
         {/*  className='absolute left-6 bottom-8 rounded-md flex flex-col items-baseline justify-between gap-3 px-3 py-4 sm:px-4 md:px-6 bg-gradient-to-b dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast'>*/}
         {/*  <h2 className="max-w-md text-3xl font-medium">*/}
@@ -16,5 +16,5 @@ export const BigBannerStatic = () => {
         {/*</div>*/}
       </div>
     </Link>
-  )
-}
+  );
+};
