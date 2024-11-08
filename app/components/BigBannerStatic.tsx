9408060f62b4c8a2d52db63dd9c17f5f069706b2
Adilet Berdibekov <berdibekovadilet@gmail.com>
2024-11-08 15:10:12 +0600

    add EFB-730

--- app/components/BigBannerStatic.tsx
+++ app/components/BigBannerStatic.tsx
@@ -1,18 +1,15 @@
 import {Link} from '~/components/Link';
-import Banner from '~/assets/banners/casio-pink-ribbon.png';
+import Banner from '~/assets/banners/efb-730.png';
 
 export const BigBannerStatic = () => {
   return (
-    <Link to="/casio-pink-ribbon">
+    <Link to="/search?q=730">
       <div className="relative overflow-hidden inline-block md:min-w-[700px] h-[531px] transition-all duration-[0.2s] ease-[ease] mb-6 hover:scale-[1.02] rounded-[20px]">
         <img src={Banner} alt="Banner" className="w-full h-full object-cover" />
-        {/*<div*/}
-        {/*  className='absolute left-6 bottom-8 rounded-md flex flex-col items-baseline justify-between gap-3 px-3 py-4 sm:px-4 md:px-6 bg-gradient-to-b dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast'>*/}
-        {/*  <h2 className="max-w-md text-3xl font-medium">*/}
-        {/*    CASIO 50 ЛЕТ*/}
-        {/*  </h2>*/}
-        {/*  <p className='text-xl font-medium'>Вне времени</p>*/}
-        {/*</div>*/}
+        <div className="absolute left-6 bottom-8 rounded-md flex flex-col items-baseline justify-between gap-3 px-3 py-4 sm:px-4 md:px-6 bg-gradient-to-b dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast">
+          <h2 className="max-w-md text-3xl font-medium">EDIFICE</h2>
+          <p className="text-xl font-medium">EFB-730</p>
+        </div>
       </div>
     </Link>
   );
