54bdc35c734a3863f425fcb8acdc987ee1ccfacf
Adilet Berdibekov <berdibekovadilet@gmail.com>
2024-12-12 13:58:47 +0600

    new year banner

--- app/components/BigBannerStatic.tsx
+++ app/components/BigBannerStatic.tsx
@@ -1,14 +1,14 @@
 import {Link} from '~/components/Link';
-import Banner from '~/assets/banners/efb-730.png';
+import Banner from '~/assets/banners/gshockgiftguide.avif';
 
 export const BigBannerStatic = () => {
   return (
-    <Link to="/search?q=730">
+    <Link to="/products">
       <div className="relative overflow-hidden inline-block md:min-w-[700px] h-[531px] transition-all duration-[0.2s] ease-[ease] mb-6 hover:scale-[1.02] rounded-[20px]">
         <img src={Banner} alt="Banner" className="w-full h-full object-cover" />
         <div className="absolute left-6 bottom-8 rounded-md flex flex-col items-baseline justify-between gap-3 px-3 py-4 sm:px-4 md:px-6 bg-gradient-to-b dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast">
-          <h2 className="max-w-md text-3xl font-medium">EDIFICE</h2>
-          <p className="text-xl font-medium">EFB-730</p>
+          <h2 className="max-w-md text-3xl font-medium">Новый год с CASIO</h2>
+          {/* <p className="text-xl font-medium">EFB-730</p> */}
         </div>
       </div>
     </Link>
