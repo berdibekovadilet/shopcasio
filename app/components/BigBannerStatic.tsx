481e67ff052fb6b4b5a530a58e7dd2a775df99b7
Adilet Berdibekov <berdibekovadilet@gmail.com>
2025-02-05 20:19:13 +0600

    valentine promo

--- app/components/BigBannerStatic.tsx
+++ app/components/BigBannerStatic.tsx
@@ -1,5 +1,5 @@
 import {Link} from '~/components/Link';
-import Banner from '~/assets/banners/gshockgiftguide.avif';
+import Banner from '~/assets/banners/valentine.png';
 
 export const BigBannerStatic = () => {
   return (
@@ -7,7 +7,9 @@ export const BigBannerStatic = () => {
       <div className="relative overflow-hidden inline-block md:min-w-[700px] h-[531px] transition-all duration-[0.2s] ease-[ease] mb-6 hover:scale-[1.02] rounded-[20px]">
         <img src={Banner} alt="Banner" className="w-full h-full object-cover" />
         <div className="absolute left-6 bottom-8 rounded-md flex flex-col items-baseline justify-between gap-3 px-3 py-4 sm:px-4 md:px-6 bg-gradient-to-b dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast">
-          <h2 className="max-w-md text-3xl font-medium">Новый год с CASIO</h2>
+          <h2 className="max-w-md text-3xl font-medium">
+            День Валентина с CASIO
+          </h2>
           {/* <p className="text-xl font-medium">EFB-730</p> */}
         </div>
       </div>
