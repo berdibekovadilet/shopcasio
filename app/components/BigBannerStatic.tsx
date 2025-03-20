3339f016c920c6369902c75d7526277bd3faf494
Adilet Berdibekov <berdibekovadilet@gmail.com>
2025-03-20 16:05:42 +0600

    nooruz banner

--- app/components/BigBannerStatic.tsx
+++ app/components/BigBannerStatic.tsx
@@ -1,15 +1,15 @@
 import {Link} from '~/components/Link';
-import Banner from '~/assets/banners/8march.png';
+import Banner from '~/assets/banners/nooruz.png';
 
 export const BigBannerStatic = () => {
   return (
-    <Link to="/casio-8march">
+    <Link to="/products">
       <div className="relative overflow-hidden inline-block md:min-w-[700px] h-[531px] transition-all duration-[0.2s] ease-[ease] mb-6 hover:scale-[1.02] rounded-[20px]">
         <img src={Banner} alt="Banner" className="w-full h-full object-cover" />
-        <div className="absolute left-6 bottom-8 rounded-md flex flex-col items-baseline justify-between gap-3 px-3 py-4 sm:px-4 md:px-6 bg-gradient-to-b dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast">
-          <h2 className="max-w-md text-3xl font-medium">8 МАРТА</h2>
-          <p className="text-xl font-medium">Международный женский день</p>
-        </div>
+        {/* <div className="absolute left-6 bottom-8 rounded-md flex flex-col items-baseline justify-between gap-3 px-3 py-4 sm:px-4 md:px-6 bg-gradient-to-b dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast">
+          <h2 className="max-w-md text-3xl font-medium">НООРУЗ</h2>
+          <p className="text-xl font-medium">майрамыныздар менен</p>
+        </div> */}
       </div>
     </Link>
   );
