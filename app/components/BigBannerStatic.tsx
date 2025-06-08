75616360fd0dcda19339553e00583f75f5612936
Adilet Berdibekov <berdibekovadilet@gmail.com>
2025-06-08 20:47:35 +0600

    add blue-phosphorescent

--- app/components/BigBannerStatic.tsx
+++ app/components/BigBannerStatic.tsx
@@ -3,7 +3,7 @@ import Banner from '~/assets/banners/nooruz.png';
 
 export const BigBannerStatic = () => {
   return (
-    <Link to="/collections/casio-x-pac-man">
+    <Link to="/collections/blue-phosphorescent">
       <div className="relative overflow-hidden inline-block md:min-w-[700px] h-[531px] transition-all duration-[0.2s] ease-[ease] mb-6 hover:scale-[1.02] rounded-[20px]">
         <img src={Banner} alt="Banner" className="w-full h-full object-cover" />
         {/* <div className="absolute left-6 bottom-8 rounded-md flex flex-col items-baseline justify-between gap-3 px-3 py-4 sm:px-4 md:px-6 bg-gradient-to-b dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast">
